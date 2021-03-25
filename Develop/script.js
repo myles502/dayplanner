702b59d8d719ed0da3351bedccd9e9ae99cccac4
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-24 22:26:21 -0700

    wedchanges

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+
+var timeTen = $("#hour-10")
+timeTen = 10 
+
+
+//compare id of text box to moment to change color 
+//find a way to use local storage for click events on class of save button 
+
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+ 
+
+
+if (timeTen === moment (); {
+$(timeTen).css ({"background-color": "blue"; color : "white" }) 
+
+}
+
