5ad190de774f3fcb62887c009515bc7e9c8b6164
myles502 <80232038+myles502@users.noreply.github.com>
2021-03-25 19:53:02 -0700

    correction

--- Develop/$('#save');.js
+++ Develop/$('#save');.js
@@ -5,3 +5,7 @@ $('#save').html(localStorage.content);
 $('.col-md-10 description')
 localStorage.content = $('.col-md-10 description').html();
 $('.col-md-10 description').html(localStorage.content);
+
+
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
--- Develop/script.js
+++ Develop/script.js
@@ -14,11 +14,11 @@ var timeG = $("#hour-15")
 timeFifteen = 15
 var timeH = $("#hour-16")
 timeSixteen = 16
-var timeI = $("hour-17")
+var timeI = $("#hour-17")
 timeSeventeen = 17
 
 
-moment().hour(); 
+ 
 
 //compare id of text box to moment to change color 
 //find a way to use local storage for click events on class of save button 
@@ -30,64 +30,64 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 if (timeNine === moment().hour()) {
 $(timeA).addClass("present");
 } else if (timeNine > moment().hour())
-$(timeA).addClass("past");
- else { $(timeA).addClass("future")
+$(timeA).addClass("future");
+ else { $(timeA).addClass("past")
 } 
     
 if (timeTen === moment().hour()) {
 $(timeB).addClass("present");
 } else if (timeTen > moment().hour())
-$(timeB).addClass("past");
- else { $(timeB).addClass("future")
+$(timeB).addClass("future");
+ else { $(timeB).addClass("past")
 } 
 
 if (timeEleven === moment().hour()) {
 $(timeC).addClass("present");
 } else if (timeEleven > moment().hour())
-$(timeC).addClass("past");
- else { $(timeC).addClass("future")
+$(timeC).addClass("future");
+ else { $(timeC).addClass("past")
 } 
     
 if (timeTwelve === moment().hour()) {
 $(timeD).addClass("present");
 } else if (timeTwelve > moment().hour())
-$(timeD).addClass("past");
-else { $(timeD).addClass("future")
+$(timeD).addClass("future");
+else { $(timeD).addClass("past")
 } 
 
 if (timeThirteen === moment().hour()) {
 $(timeE).addClass("present");
 } else if (timeThirteen > moment().hour())
-$(timeE).addClass("past");
-else { $(timeE).addClass("future")
+$(timeE).addClass("future");
+else { $(timeE).addClass("past")
 } 
 
 if (timeFourteen === moment().hour()) {
 $(timeF).addClass("present");
 } else if (timeFourteen > moment().hour())
-$(timeF).addClass("past");
-else { $(timeF).addClass("future")
+$(timeF).addClass("future");
+else { $(timeF).addClass("past")
 } 
 
 if (timeFifteen === moment().hour()) {
 $(timeG).addClass("present");
 } else if (timeFifteen > moment().hour())
-$(timeG).addClass("past");
-else { $(timeG).addClass("future")
+$(timeG).addClass("future");
+else { $(timeG).addClass("past")
 } 
 
 if (timeSixteen === moment().hour()) {
 $(timeH).addClass("present");
 } else if (timeSixteen > moment().hour())
-$(timeH).addClass("past");
-else { $(timeH).addClass("future")
+$(timeH).addClass("future");
+else { $(timeH).addClass("past")
 } 
 
 if (timeSeventeen === moment().hour()) {
 $(timeI).addClass("present");
 } else if (timeSeventeen > moment().hour())
-$(timeI).addClass("past");
-else { $(timeI).addClass("future")
+$(timeI).addClass("future");
+else { $(timeI).addClass("past")
 } 
     
     
